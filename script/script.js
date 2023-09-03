ee744f49aabc1e743ea5eb76b691ef9ff961b310
Vitor Goulart <vitorgoulartofficial@gmail.com>
2023-09-03 18:20:20 -0300

    ajustes

--- script/script.js
+++ script/script.js
@@ -1,13 +1,23 @@
-const Mario = document.querySelector('.Mario')
+const Mario = document.querySelector(".Mario")
+const Cano = document.querySelector(".Cano")
 
 const pulo = function () {
-    Mario.classList.add("pulo");
-    setTimeout (function () {
-    Mario.classList.remove("pulo");
-}, 500);
+  Mario.classList.add("pulo")
+  setTimeout(function () {
+    Mario.classList.remove("pulo")
+  }, 500)
 }
 
+const loop = setInterval(() => {
+  const posiçãodocan = Cano.offsetLeft
+  const posiçãodomario = +window
+    .getComputedStyle(Mario)
+    .bottom.replace("px", " ")
 
+  if (posiçãodocan <= 120 && posiçãodomario < 80) {
+    Cano.style.animation = "none"
+    Cano.style.left = "${posiçãodocano}px"
+  }
+}, 10)
 
-
-document.addEventListener('keydown', pulo);
+document.addEventListener("keydown", pulo)
